2bc382a014458beda2c5dc86ec452367a6c79c35
Luke <rohenaz@gmail.com>
2023-04-10 11:17:05 -0400

    just combined the files and be done w it

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,67 @@
-import { BpuTx, ParseConfig } from "./types/common";
 export declare const parse: (o: ParseConfig, config?: any) => Promise<BpuTx>;
+export type Cell = {
+    op?: number;
+    ops?: string;
+    b?: string;
+    s?: string;
+    ii: number;
+    i: number;
+    h?: string;
+    f?: string;
+    ls?: string;
+    lh?: string;
+    lf?: string;
+    lb?: string;
+};
+export type Tape = {
+    cell: Cell[];
+    i: number;
+};
+export type Out = {
+    tape: Tape[];
+    i: number;
+    e: {
+        i: number;
+        a: string | false;
+        v: number;
+    };
+};
+export type In = {
+    i: number;
+    e: {
+        h: string;
+        a: string;
+        v?: number;
+    };
+};
+export type BpuTx = {
+    out: Out[];
+    in?: In[];
+    tx: {
+        h?: string;
+        r?: string;
+    };
+    lock?: number;
+};
+export type ByRawTx = {
+    r: string;
+};
+export type ByTxId = {
+    h: string;
+};
+export type SplitConfig = {
+    token: {
+        op?: number;
+        ops?: string;
+        s?: string;
+        b?: string;
+    };
+    include?: string;
+};
+export type ParseConfig = {
+    tx: ByRawTx | ByTxId;
+    split?: SplitConfig[];
+    transform?: (o: any, c: any) => Object;
+};
 export type { Config } from "bitcoind-rpc";
 //# sourceMappingURL=index.d.ts.map
